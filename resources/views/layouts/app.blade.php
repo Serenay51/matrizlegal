8d2090d3a6577f8ab2f4fdab8bcf67f2d670d4b8
Serenay51 <lautyh98@gmail.com>
2025-04-07 12:56:45 -0300

    Add: Premium and No Premium span next to user name in navigation.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         @stack('script')
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
